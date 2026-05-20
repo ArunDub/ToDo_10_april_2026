88d02148377f5b018224906cfb376dcb0aa20473
Arun Dubey <arundubey8381@outlook.com>
2026-05-20 08:01:30 +0530

    20_may_2026

---
+++ Domain/Interfaces/IRepository.cs
@@ -0,0 +1,16 @@
+﻿namespace Domain.Interfaces
+{
+    public interface IRepository<Tmodel> where Tmodel : class
+    {
+        Task<IEnumerable<Tmodel>> Get();
+        Task<Tmodel> Get(int Id);
+        void Create(Tmodel model);
+        void Update(Tmodel model);
+        void Delete(int Id);
+
+        void CreateRange(IEnumerable<Tmodel> models);
+        void UpdateRange(IEnumerable<Tmodel> models);
+        void DeleteRange(IEnumerable<Tmodel> models); 
+
+    }
+}
---
+++ Domain/Models/TodoGroup.cs
@@ -0,0 +1,9 @@
+﻿namespace Domain.Models
+{
+    public class TodoGroup
+    {
+        public int Id { get; set; }
+        public string GroupName { get; set; }
+        public DateTime CreatedOn { get; set; }
+    }
+}
