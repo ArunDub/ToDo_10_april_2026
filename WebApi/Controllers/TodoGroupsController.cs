915393e9788f6172e59681d6ab0d84c48a70830f
Arun Dubey <arundubey8381@outlook.com>
2026-05-28 08:18:22 +0530

    To do Group Controller Complete

--- WebApi/Controllers/TodoGroupsController.cs
+++ WebApi/Controllers/TodoGroupsController.cs
@@ -61,8 +61,8 @@ namespace WebApi.Controllers
                     foreach (var error in errors)
                     {
                         msgBuilder.AppendLine(error.ToString());
-                        return BadRequest(ApiResponseBuilder.GenerateBadRequest("Create Failed", msgBuilder.ToString()));
                     }
+                    return BadRequest(ApiResponseBuilder.GenerateBadRequest("Create Failed", msgBuilder.ToString()));
                 }
             }
 
@@ -89,8 +89,8 @@ namespace WebApi.Controllers
                     foreach (var error in errors)
                     {
                         msgBuilder.AppendLine(error.ToString());
-                        return BadRequest(ApiResponseBuilder.GenerateBadRequest("Update failed", msgBuilder.ToString()));
                     }
+                    return BadRequest(ApiResponseBuilder.GenerateBadRequest("Update failed", msgBuilder.ToString()));
                 }
             }
             var updatedDto = await _service.TodoGroupService.Update(modelDto);
@@ -115,6 +115,98 @@ namespace WebApi.Controllers
                 return BadRequest(ApiResponseBuilder.GenerateBadRequest("Delete Failed", "There might be active child record(s)"));
             }
             return Ok(ApiResponseBuilder.GenerateOK(rowsAffected, 200, "Ok", $"Record with id {id} Deleted"));
+        }
+        //POST TodoGroups/CreateRange
+        [HttpPost("CreateRange")]
+        public async Task<IActionResult> CreatRange([FromBody] List<TodoGroupDto> modelDto)
+        {
+            if (modelDto == null)
+                return BadRequest(ApiResponseBuilder.GenerateBadRequest("Bulk create failed", "Input is null"));
+            if (!ModelState.IsValid)
+            {
+                var errors = ModelState.GetModelStateErrors();
+                if (errors != null && errors.Count > 0)
+                {
+                    var msgBuilder = new StringBuilder();
+                    foreach (var error in errors)
+                    {
+                        msgBuilder.AppendLine(error.ToString());
+                    }
+                    return BadRequest(ApiResponseBuilder.GenerateBadRequest("Bulk Create Failed", msgBuilder.ToString()));
+                }
+            }
+            var createdDtos = await _service.TodoGroupService.CreateRange(modelDto);
+            if (createdDtos == null || createdDtos.Count <= 0)
+            {
+                return BadRequest(ApiResponseBuilder.GenerateBadRequest("Bulk Create Failed", "Some errors occured"));
+            }
+            else
+            {
+                return Ok(ApiResponseBuilder.GenerateOK(createdDtos, 200, "OK", "Bulk Create Success"));
+            }
+        }
+        //POST TodoGroups/UpdateRange
+        [HttpPost("UpdateRange")]
+        public async Task<IActionResult> UpdateRange([FromBody] List<TodoGroupDto> modelDtos)
+        {
+            if (modelDtos == null || modelDtos.Count <= 0)
+            {
+                return BadRequest(ApiResponseBuilder.GenerateBadRequest("Bulk Update Failed", "input is null"));
+            }
+            if (!ModelState.IsValid)
+            {
+                var errors = ModelState.GetModelStateErrors();
+                if (errors != null)
+                {
+                    var msgBulder = new StringBuilder();
+
+                    foreach (var eror in errors)
+                    {
+                        msgBulder.AppendLine(eror.ToString());
+                    }
+                    return BadRequest(ApiResponseBuilder.GenerateBadRequest("Bulk Update Failed", msgBulder.ToString()));
+                }
+            }
+            var updatedDto = await _service.TodoGroupService.UpdateRange(modelDtos);
+            if (updatedDto == null || updatedDto.Count <= 0)
+            {
+                return BadRequest(ApiResponseBuilder.GenerateBadRequest("Bulk Update Failed", "some error occured"));
+            }
+            else
+            {
+                return Ok(ApiResponseBuilder.GenerateOK(updatedDto, 200, "Ok", "Bulk Update Success"));
+            }
+        }
+        //POST TodoGroups/DeleteRange
+        [HttpPost("DeleteRange")]
+        public async Task<IActionResult> DeleteRange([FromBody] List<TodoGroupDto> modelDtos)
+        {
+            if (modelDtos == null || modelDtos.Count <= 0)
+            {
+                return BadRequest(ApiResponseBuilder.GenerateBadRequest("Bulk Delete Failed", "Input is null"));
+            }
+            if (!ModelState.IsValid)
+            {
+                var errors = ModelState.GetModelStateErrors();
+                if (errors != null)
+                {
+                    var msgBuiler = new StringBuilder();
+                    foreach (var error in errors)
+                    {
+                        msgBuiler.AppendLine(error.ToString());
+                    }
+                    return BadRequest(ApiResponseBuilder.GenerateBadRequest("Bulk Delete Failed", msgBuiler.ToString()));
+                }
+            }
+            var rowsAffected = await _service.TodoGroupService.DeleteRange(modelDtos);
+            if (rowsAffected <= 0)
+            {
+                return BadRequest(ApiResponseBuilder.GenerateBadRequest("Bulk Update Failed", "There might be some active child record"));
+            }
+            else
+            {
+                return Ok(ApiResponseBuilder.GenerateOK(rowsAffected, 200, "OK", "Bulk delete success"));
+            }
         }
     }
 }
